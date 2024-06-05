0a574aa3ca137d8cb7af35d2d560736fc03bd164
xandelol <aacds@msn.com>
2024-06-05 13:58:38 -0300

    CRUD: ler arquivos

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -1,4 +1,6 @@
 import 'package:personal_tasks/components/task.dart';
+import 'package:personal_tasks/data/database.dart';
+import 'package:sqflite/sqflite.dart';
 
 class TaskDao {
   static const String taskTable = 'CREATE TABLE $_tableName('
@@ -13,9 +15,29 @@ class TaskDao {
 
   save(Task task) async {}
 
-  Future<List<Task>> findAll() async {}
+  Future<List<Task>> findAll() async {
+    final Database db = await getDatabase();
+    final List<Map<String, dynamic>> tasksMap = await db.query(_tableName);
+    return toTaskList(tasksMap);
+  }
 
-  Future<List<Task>> find(String taskName) async {}
+  Future<List<Task>> find(String taskName) async {
+    final Database db = await getDatabase();
+    final List<Map<String, dynamic>> tasksMap =
+        await db.query(_tableName, where: '$_name = ?', whereArgs: [taskName]);
+    return toTaskList(tasksMap);
+  }
 
   delete(String taskName) async {}
+
+  List<Task> toTaskList(List<Map<String, dynamic>> mapList) {
+    final List<Task> tasks = [];
+    for (Map<String, dynamic> map in mapList) {
+      tasks.add(mapToTask(map));
+    }
+    return tasks;
+  }
+
+  Task mapToTask(Map<String, dynamic> map) =>
+      Task(map[_name], map[_image], map[_difficulty]);
 }
